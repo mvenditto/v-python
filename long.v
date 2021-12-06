8c1ebf0e1aa1102bd4a7a4e215e9787b7dde75a2
mvenditto <venditto.matteo4@gmail.com>
2021-12-06 20:55:22 +0000

    add long

---
+++ long.v
@@ -0,0 +1,38 @@
+module py
+
+// longobject.h
+
+[typedef]
+pub struct C.PyLongObject { }
+
+// macro
+pub fn C.PyLong_Check(&C.PyObject) int
+pub fn C.PyLong_CheckExact(&C.PyObject) int
+
+// PyAPI_FUNC
+pub fn C.PyLong_FromLong(int) &C.PyObject
+pub fn C.PyLong_FromUnsignedLong(u64) &C.PyObject
+pub fn C.PyLong_FromSize_t(&C.size_t) &C.PyObject
+pub fn C.PyLong_FromSsize_t(&C.Py_ssize_t) &C.PyObject
+pub fn C.PyLong_FromDouble(f64) &C.PyObject
+pub fn C.PyLong_AsLong(&C.PyObject) int
+pub fn C.PyLong_AsLongAndOverflow(&C.PyObject, int) int
+pub fn C.PyLong_AsSsize_t(&C.PyObject) &C.Py_ssize_t
+pub fn C.PyLong_AsSize_t(&C.PyObject) &C.size_t
+pub fn C.PyLong_AsUnsignedLong(&C.PyObject) u32
+pub fn C.PyLong_AsUnsignedLongMask(&C.PyObject) u32
+pub fn C.PyLong_GetInfo(voidptr) &C.PyObject
+pub fn C.PyLong_AsDouble(&C.PyObject) f64
+pub fn C.PyLong_FromVoidPtr(voidptr) &C.PyObject
+pub fn C.PyLong_AsVoidPtr(&C.PyObject) voidptr
+pub fn C.PyLong_FromLongLong(i64) &C.PyObject
+pub fn C.PyLong_FromUnsignedLongLong(u64) &C.PyObject
+pub fn C.PyLong_AsLongLong(&C.PyObject) i64
+pub fn C.PyLong_AsUnsignedLongLong(&C.PyObject) u64
+pub fn C.PyLong_AsUnsignedLongLongMask(&C.PyObject) u64
+pub fn C.PyLong_AsLongLongAndOverflow(&C.PyObject, int)i64
+pub fn C.PyLong_FromString(&char, &&char, int) &C.PyObject
+
+// mystrtoul.c
+pub fn C.PyOS_strtoul(&char, &&char, &int) u64
+pub fn C.PyOS_strtol(&char, &&char, &int) i64
---
+++ long_test.v
@@ -0,0 +1,85 @@
+module py
+
+#flag -L/usr/lib/python3.9/config-3.9-x86_64-linux-gnu 
+#flag -L/usr/lib/x86_64-linux-gnu
+#flag -lcrypt 
+#flag -lpython3.9
+#flag -ldl 
+#flag -lm
+
+fn testsuite_begin() {
+	C.Py_Initialize()
+}
+
+fn testsuite_end() {
+}
+
+fn test_long_check() {
+	l := C.PyLong_FromLong(4242)
+	assert C.PyLong_Check(l) == 1
+	assert C.PyLong_CheckExact(l) == 1
+}
+
+fn test_long_from_long() {
+	val := int(4242)
+	py_l := C.PyLong_FromLong(val)
+	assert voidptr(py_l) != C.NULL
+	v_l := C.PyLong_AsLong(py_l)
+	assert v_l == val
+	C.Py_XDECREF(py_l)
+}
+
+fn test_long_from_ulong() {
+	val := u32(4242)
+	py_l := C.PyLong_FromUnsignedLong(val)
+	assert voidptr(py_l) != C.NULL
+	v_l := C.PyLong_AsUnsignedLong(py_l)
+	assert v_l == val
+	C.Py_XDECREF(py_l)
+}
+
+fn test_long_from_long_long() {
+	val := i64(4242)
+	py_l := C.PyLong_FromLongLong(val)
+	assert voidptr(py_l) != C.NULL
+	v_l := C.PyLong_AsLongLong(py_l)
+	assert v_l == val
+	C.Py_XDECREF(py_l)
+}
+
+fn test_long_from_u_long_long() {
+	val := u64(4242)
+	py_l := C.PyLong_FromUnsignedLongLong(val)
+	assert voidptr(py_l) != C.NULL
+	v_l := C.PyLong_AsUnsignedLongLong(py_l)
+	assert v_l == val
+	C.Py_XDECREF(py_l)
+}
+
+fn test_long_from_float() {
+	val := f64(4242)
+	py_l := C.PyLong_FromDouble(val)
+	assert voidptr(py_l) != C.NULL
+	v_l := C.PyLong_AsDouble(py_l)
+	assert v_l == val
+	C.Py_XDECREF(py_l)
+}
+
+fn test_long_from_string() {
+	val := 4242
+	val_str := val.str()
+	py_l := C.PyLong_FromString(val_str.str, C.NULL, 10)
+	assert voidptr(py_l) != C.NULL
+	v_l := C.PyLong_AsLong(py_l)
+	assert v_l == val
+	C.Py_XDECREF(py_l)
+}
+
+fn test_long_from_voidptr() {
+	val := 4242
+	py_l := C.PyLong_FromVoidPtr(voidptr(val))
+	assert voidptr(py_l) != C.NULL
+	v_l := C.PyLong_AsLong(py_l)
+	assert v_l == val
+	C.Py_XDECREF(py_l)
+}
