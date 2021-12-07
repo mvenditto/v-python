d885c97e44a6a60a314afbdc22c98d1f2c6f2e6b
mvenditto <venditto.matteo4@gmail.com>
2021-12-07 09:09:52 +0000

    alternatives to pass cc flags

--- helpers/cc_env.sh
+++ helpers/cc_env.sh
@@ -4,3 +4,11 @@
 # source _cc_env.sh --embed (links libpython)
 export CFLAGS=$(python3-config --cflags)
 export LDFLAGS=$(python3-config --ldflags $1)
+
+#export CFLAGS=$(pkg-config python-3.9-embed --cflags)
+#export LDFLAGS=$(pkg-config python-3.9-embed --libs)
+
+#export CFLAGS=$(pkg-config python3-embed --cflags)
+#export LDFLAGS=$(pkg-config python3-embed --libs)
+
+#export CFLAGS=$(pkg-config python3 --cflags)
