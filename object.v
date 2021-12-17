8c5fc48474e91b5afd062a278d62bbceceaddac4
mvenditto <venditto.matteo4@gmail.com>
2021-12-17 11:12:54 +0000

    added Py_NotImplemented

--- object.v
+++ object.v
@@ -2,13 +2,26 @@ module py
 
 pub struct C.PyObject { }
 
+const (
+	py_not_implemented = &C.PyObject(C.Py_NotImplemented)
+)
+
+// object protocol
 pub fn C.PyObject_Str(&C.PyObject) &C.PyObject
+pub fn C.PyObject_IsInstance(&C.PyObject, &C.PyObject) int
+pub fn C.PyObject_GetAttrString(&C.PyObject, &char) &C.PyObject
+
+// call protcol
 pub fn C.PyObject_CallObject(&C.PyObject, &C.PyObject) &C.PyObject
 pub fn C.PyObject_CallFunctionObjArgs(&C.PyObject, ...&C.PyObject) &C.PyObject
 pub fn C.PyObject_CallMethodObjArgs(&C.PyObject, &C.PyObject, ...&C.PyObject) &C.PyObject
 pub fn C.PyObject_CallMethod(&C.PyObject, &char, &char, ...&C.PyObject) &C.PyObject
-pub fn C.PyObject_IsInstance(&C.PyObject, &C.PyObject) int
-pub fn C.PyObject_GetAttrString(&C.PyObject, &char) &C.PyObject
+
+[inline]
+pub fn py_not_implemented_ret() &C.PyObject { 
+	C.Py_INCREF(C.Py_NotImplemented)
+	return &C.PyObject(C.Py_NotImplemented)
+}
 
 [inline]
 pub fn (o &C.PyObject) ptr() voidptr {
